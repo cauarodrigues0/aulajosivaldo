5a3e59d8d7188547dd8e5a64bdc545bd5f6095fd
cauarodrigues0 <caua.rodrigues7435@gmail.com>
2024-04-10 17:09:12 -0300

    criado o arquivo principal onde irá utilizar os objetos

--- pessoa.py
+++ pessoa.py
@@ -11,13 +11,3 @@ if __name__ == "__main__":
     pessoa1 = Pessoa("João", 33, "12345678-90")
     pessoa2= Pessoa("Maria", "23", 241241421)
 
-print("nome:",pessoa1.nome)
-print("idade:",pessoa1.idade)
-print("CPF:",pessoa1.cpf)
-
-print("nome:",pessoa2.nome)
-print("idade:",pessoa2.idade)
-print("CPF:",pessoa2.cpf)
-
-print(pessoa1.comer())
-print(pessoa2.comer())
