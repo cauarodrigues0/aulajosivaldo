1648597da76d899f80da95f215af2b3d3ead61b4
cauarodrigues0 <caua.rodrigues7435@gmail.com>
2024-04-10 17:23:31 -0300

    criado os métodos beber, falar e correr. e removido o arquivo dos objetos

--- carro.py
+++ carro.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     carro1 = Carro("ABC-1234", "Gol", 2010)
     carro2 = Carro("WAD-4721", "Fusca", 1980)
 
-
+print(carro1.ano,carro1.modelo,carro1.placa)
+print(carro2.ano,carro2.modelo,carro2.placa)
--- pessoa.py
+++ pessoa.py
@@ -6,8 +6,38 @@ class Pessoa():
     
     def comer(self):
         return f"{self.nome} está comendo!"
+    
+    def beber(self):
+        return f"{self.nome} está bebendo!"
+    
+    def falar(self, mensagem):
+        return f"{self.nome} está falando: {mensagem}"
+    
+    def correr(self):
+        return f"{self.nome} está correndo!"
 
 if __name__ == "__main__":
     pessoa1 = Pessoa("João", 33, "12345678-90")
     pessoa2= Pessoa("Maria", "23", 241241421)
+
+print("nome:",pessoa1.nome)
+print("idade:",pessoa1.idade)
+print("CPF:",pessoa1.cpf)
+
+print("nome:",pessoa2.nome)
+print("idade:",pessoa2.idade)
+print("CPF:",pessoa2.cpf)
+
+print(pessoa1.comer())
+print(pessoa2.comer())
+
+print(pessoa1.beber())
+print(pessoa2.beber())
+
+print(pessoa1.falar("bom dia"))
+print(pessoa2.falar("boa tarde"))
+
+print(pessoa1.correr())
+print(pessoa2.correr())
+
 
