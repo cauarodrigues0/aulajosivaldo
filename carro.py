779034e016d3c8aca4e3d606962da0ec4ef60da4
cauarodrigues0 <caua.rodrigues7435@gmail.com>
2024-04-10 16:57:18 -0300

    criado os objetos da classe carro

--- carro.py
+++ carro.py
@@ -3,3 +3,8 @@ class Carro():
         self.placa = placa
         self.modelo = modelo
         self.ano = ano
+if __name__ == "__main__":
+    carro1 = Carro("ABC-1234", "Gol", 2010)
+    carro2 = Carro("WAD-4721", "Fusca", 1980)
+
+
